1d9e70f4958235c7733ec53ecb159e49374cde3d
Othneil Victory <othneilvictory16@gmail.com>
2024-01-19 13:15:34 +0100

    MY FIRST BANK API

--- controllers/userCont.js
+++ controllers/userCont.js
@@ -142,39 +142,38 @@ exports.signIn = async (req, res) => {
 }
 
 
-
-
 exports.signOut = async (req, res) => {
-    try{
-        const {userId} = req.user
-        const {token} = req.user
+    try {
+        const { userId, token } = req.user;
         const user = await userModel.findById(userId);
 
-    if(!user){
+        if (!user) {
             return res.status(404).json({
-                message: 'This user not found'
-            })
+                message: 'User not found'
+            });
         }
-    
-         if (!user.token) {
+
+        if (!user.token || user.token !== token) {
             return res.status(400).json({
                 message: 'User does not have a valid token'
             });
         }
-       // Revoke token by setting its expiration to a past date
-       const decodedToken = jwt.verify(user.token, process.env.secret);
-       decodedToken.exp = 1;
 
-   return res.status(200).json({
-        message: 'You have signed out successfully'
-    })
-        
-        
-    }catch(err){
+        // Invalidate the token by setting its expiration to a past date
+        const pastDate = new Date(0);
+        const newToken = jwt.sign({}, process.env.secret, { expiresIn: pastDate });
+
+        // Update the user's token with the new invalidated token
+        user.token = newToken;
+        await user.save();
+
+        return res.status(200).json({
+            message: 'You have signed out successfully'
+        });
+
+    } catch (err) {
         return res.status(500).json({
             message: err.message
-        })
+        });
     }
-}
-
-
+};
