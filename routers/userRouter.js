0dd1f589163ca4f94fd30bd9087b011447f44487
Othneil Victory <othneilvictory16@gmail.com>
2024-01-24 15:50:15 +0100

    second commit

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -14,36 +14,36 @@ const userRouter = require("express").Router()
 
 // for user signing-up into the bank app
 // router.post('/sign-up', uploads.single('profileImage'), signUp)
-userRouter.post('/sign-up', signUp)
+userRouter.post('user/sign-up', signUp)
 
 // for user logging-in into the bank app
-userRouter.post('/sign-in', signIn)
+userRouter.post('user/sign-in', signIn)
 
 
 // for user logging-out into the bank app
-userRouter.post('/sign-out',authenticate, signOut)
+userRouter.post('user/sign-out',authenticate, signOut)
 
 
 
 // for user depositing into his account
-userRouter.post ("/deposit",authenticate, deposit)
+userRouter.post ("user/deposit",authenticate, deposit)
 
 // for user traansfering out of the account
-userRouter.post("/transfer", authenticate,transfer)
+userRouter.post("user/transfer", authenticate,transfer)
 
 // for user withdrawing into another account
-userRouter.post("/withdraw", authenticate, withdraw)
+userRouter.post("user/withdraw", authenticate, withdraw)
 
 // for the user to see all transaction history
-userRouter.get("/getallmyhistory", authenticate, getTransactionHistory)
+userRouter.get("user/getallmyhistory", authenticate, getTransactionHistory)
 
 // user pay for airtime
-userRouter.post('/purchase-airtime', authenticate, airtimeTrans)
+userRouter.post('user/purchase-airtime', authenticate, airtimeTrans)
 
 // user pay for bet
-userRouter.post('/place-bet', authenticate, betTrans)
+userRouter.post('user/place-bet', authenticate, betTrans)
 
 // user pay for electricity
-userRouter.post('/pay-electbill', authenticate, elecTrans)
+userRouter.post('user/pay-electbill', authenticate, elecTrans)
 
 module.exports = userRouter
